10fab0aa192cd16966103ea4db4204106641e507
Abhinav <hemrajjain1999@gmail.com>
2018-06-24 20:16:38 +0530

    fixed redux test errors

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () => {
-  return <div>CommentList</div>;
-};
+class CommentList extends Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
+export default CommentList;
