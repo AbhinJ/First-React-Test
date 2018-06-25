c88bfcfef5411a931fe8b00b286ae9fc5163daef
Abhinav <hemrajjain1999@gmail.com>
2018-06-25 11:42:30 +0530

    added api

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from "reducers";
+import reduxPromise from "redux-promise";
 
 export default ({ children, initialState = {} }) => {
   return (
-    <Provider store={createStore(reducers, initialState)}>{children}</Provider>
+    <Provider
+      store={createStore(reducers, initialState, applyMiddleware(reduxPromise))}
+    >
+      {children}
+    </Provider>
   );
 };
